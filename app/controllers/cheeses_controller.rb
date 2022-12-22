e8be7e1fbb3905e39aff1eeea86ef251e5e7566c
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-12-22 11:16:02 +1100

    understand custom json rendering

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -9,7 +9,13 @@ class CheesesController < ApplicationController
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    #when using find_by, if the record is not found, nil is returned
+    #nil is a falsey value, so we can check for cheese and write a custom error message if a request was made for a cheese that was not found
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: {error: "Cheese not found"}, status: :not_found
+    end
   end
 
 end
